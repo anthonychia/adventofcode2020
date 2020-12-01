8d06f4928fb1de426358df34ab86196540acb62b
Chung <39647211+anthonychia@users.noreply.github.com>
2020-12-01 13:17:38 +0000

    day 1

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = np.genfromtxt('input.txt')
+
+# Part 1
+
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        if a[i]+a[j] == 2020: 
+            print(a[i]*a[j])
+            break
+        
+# Part 2
+
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        for k in range(j,len(a)):
+            if a[i]+a[j]+a[k] == 2020: 
+                print(a[i]*a[j]*a[k])
+                break
