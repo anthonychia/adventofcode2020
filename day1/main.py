bd28c3fe334f9e9c8e5c570da2d12e04966de08d
Chung <39647211+anthonychia@users.noreply.github.com>
2020-12-02 17:36:06 +0000

    formatting

--- day1/main.py
+++ day1/main.py
@@ -5,16 +5,16 @@ a = np.genfromtxt('input.txt')
 # Part 1
 
 for i in range(len(a)):
-    for j in range(i,len(a)):
-        if a[i]+a[j] == 2020: 
-            print(a[i]*a[j])
+    for j in range(i, len(a)):
+        if a[i] + a[j] == 2020:
+            print(a[i] * a[j])
             break
-        
+
 # Part 2
 
 for i in range(len(a)):
-    for j in range(i,len(a)):
-        for k in range(j,len(a)):
-            if a[i]+a[j]+a[k] == 2020: 
-                print(a[i]*a[j]*a[k])
+    for j in range(i, len(a)):
+        for k in range(j, len(a)):
+            if a[i] + a[j] + a[k] == 2020:
+                print(a[i] * a[j] * a[k])
                 break
