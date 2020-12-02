c9401bd8b9def570d837f8f2cb078d3636c04e59
Chung <39647211+anthonychia@users.noreply.github.com>
2020-12-02 17:33:02 +0000

    minor update

--- day2/main.py
+++ day2/main.py
@@ -5,10 +5,10 @@ from collections import Counter
 
 regexp = r'([0-9]+)-([0-9]+) ([a-zA-Z]): ([a-zA-Z0-9]+)'
 output = np.fromregex('input.txt', regexp, 
-                    [('min', np.int64), ('max', np.int64), ('letter', 'U1'), ('password', 'object')])
+                        [('min', np.int64), ('max', np.int64), ('letter', 'U1'), ('password', 'object')])
                     
-print(sum([ (Counter(password)[letter] >= min) & (Counter(password)[letter] <= max) for min, max, letter, password in output ]))
+print(sum([(Counter(password)[letter] >= min) & (Counter(password)[letter] <= max) for min, max, letter, password in output]))
 
 # Part 2
 
-print(sum([ (password[pos1-1] == letter) != (password[pos2-1] == letter) for pos1, pos2, letter, password in output ]))
+print(sum([(password[pos1-1] == letter) != (password[pos2-1] == letter) for pos1, pos2, letter, password in output]))
