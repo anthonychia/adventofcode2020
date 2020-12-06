b37af799950ae752ec4af220662f07e2fdc3c6b1
Chung <39647211+anthonychia@users.noreply.github.com>
2020-12-06 09:20:06 +0000

    day 6

---
+++ day6/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+from collections import Counter
+
+groups = [[set(person) for person in line.split('\n')] for line in open('input.txt').read().split('\n\n')]
+
+# Part 1
+print(sum(len(set.union(*group)) for group in groups))
+
+# Part 2
+print(sum(len(set.intersection(*group)) for group in groups))
